f32dfe9bb084bbbfe0f2933f69264b391687c430
Gal Winer <gal@classiq.io>
2023-03-22 18:31:52 +0200

    Pushing a WIP (pre-fligh):changed particle container variant enum to a struct, working on propagating change to implemented methods.

--- src/quad_tree.rs
+++ src/quad_tree.rs
@@ -1,11 +1,15 @@
 extern crate nannou;
+
 use rand_distr::{Normal, Distribution};
 
 use nannou::prelude::*;
 use nannou::rand;
+use nannou::winit::window::CursorIcon::Default;
 
 const CAPACITY: usize = 4;
-const WINDOW_SIZE:f32=800.0;
+const WINDOW_SIZE: f32 = 800.0;
+const NDIMS: usize = 2;
+const ORTHANT_NUM: usize = usize::pow(2, NDIMS as u32);
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Particle {
@@ -16,8 +20,8 @@ pub struct Particle {
 }
 
 impl Particle {
-    pub fn new(x:f32,y:f32) -> Self {
-        let position = Point2::new(x,y);
+    pub fn new(x: f32, y: f32) -> Self {
+        let position = Point2::new(x, y);
         let velocity = Vec2::new(1.0, 0.0);
         let acceleration = Vec2::new(0.0, 0.0);
         let mass = 1.0;
@@ -29,10 +33,9 @@ impl Particle {
         }
     }
     pub fn new_random() -> Self {
-
-        let normal = Normal::new(0.0, WINDOW_SIZE/2.0).unwrap();
+        let normal = Normal::new(0.0, WINDOW_SIZE / 2.0).unwrap();
         let random_sample = || normal.sample(&mut rand::thread_rng());
-        let position = Point2::new(random_sample(),random_sample());
+        let position = Point2::new(random_sample(), random_sample());
         let velocity = Vec2::new(1.0, 0.0);
         let acceleration = Vec2::new(0.0, 0.0);
         let mass = 1.0;
@@ -44,13 +47,12 @@ impl Particle {
         }
     }
     pub fn draw(&self, draw: &Draw) {
-
         draw.ellipse()
             .x_y(self.position.x, self.position.y)
             .w_h(5.0, 5.0)
-            .rgba(102.0/255.0, 255.0/255.0, 0.0/255.0, 0.4)
+            .rgba(102.0 / 255.0, 255.0 / 255.0, 0.0 / 255.0, 0.4)
             .stroke(rgba(0.0, 0.0, 0.0, 1.0));
-            // .stroke_weight(2.0);
+        // .stroke_weight(2.0);
     }
     pub fn update(&mut self) {
         self.position += self.velocity;
@@ -84,6 +86,20 @@ impl Boundary {
         }
         false
     }
+    pub fn intersects(self, range: &Boundary) -> bool {
+        let x1 = self.center.x - self.width / 2.0;
+        let x2 = self.center.x + self.width / 2.0;
+        let y1 = self.center.y - self.height / 2.0;
+        let y2 = self.center.y + self.height / 2.0;
+        let x3 = range.center.x - range.width / 2.0;
+        let x4 = range.center.x + range.width / 2.0;
+        let y3 = range.center.y - range.height / 2.0;
+        let y4 = range.center.y + range.height / 2.0;
+        if x1 > x4 || x2 < x3 || y1 > y4 || y2 < y3 {
+            return false;
+        }
+        true
+    }
 
     pub fn draw(&self, draw: &Draw) {
         draw.rect()
@@ -95,99 +111,85 @@ impl Boundary {
     }
 }
 
-#[derive(PartialEq, Debug)]
-enum ParticleContainer {
-    Particles([Option<Particle>; CAPACITY]),
-    Divided,
+//
+// #[derive(PartialEq, Debug)]
+// enum ParticleContainer {
+//     Particles([Option<Particle>; CAPACITY]),
+//     Divided,
+// }
+#[derive(Debug)]
+pub struct ParticleContainer {
+    particles: [Option<Particle>; CAPACITY],
+    sub_trees: Vec<Option<Box<QuadTree>>>,
 }
+
+impl ParticleContainer {
+    pub fn new() -> Self {
+        Self {
+            particles: [None; CAPACITY],
+            sub_trees: Vec::with_capacity(ORTHANT_NUM),
+        }
+    }
+}
+
 
 #[derive(Debug)]
 pub struct QuadTree {
     boundary: Boundary,
     particle_container: ParticleContainer,
-    north_west: Option<Box<QuadTree>>,
-    north_east: Option<Box<QuadTree>>,
-    south_west: Option<Box<QuadTree>>,
-    south_east: Option<Box<QuadTree>>,
 }
 
 impl QuadTree {
     pub fn new(boundary: Boundary) -> Self {
         Self {
             boundary,
-            particle_container: ParticleContainer::Particles(Default::default()),
-            north_west: None,
-            north_east: None,
-            south_west: None,
-            south_east: None,
+            particle_container: ParticleContainer::new(),
         }
     }
+    pub fn new_sub_tree(&mut self, boundary: Boundary)->Self{
+        Self {
+            Boundary::new(boundary.center, boundary.width / 2.0, boundary.height / 2.0)
+            particle_container: ParticleContainer::new(),
+        }
+    }
+
     pub fn draw(&self, draw: &Draw) {
         self.boundary.draw(draw);
-
-        match self.particle_container {
-            ParticleContainer::Particles(particles) => {
-                for particle in particles.iter().filter(|p| p.is_some()).map(|p| p.unwrap()) {
-                    particle.draw(draw);
-                }
+        self.particle_container.particles.iter().for_each(|p| {
+            if let Some(p) = p {
+                p.draw(draw);
             }
-            ParticleContainer::Divided => {
-                self.north_west.as_ref().unwrap().draw(draw);
-                self.north_east.as_ref().unwrap().draw(draw);
-                self.south_west.as_ref().unwrap().draw(draw);
-                self.south_east.as_ref().unwrap().draw(draw);
+        });
+        self.particle_container.sub_trees.iter().for_each(|t| {
+            if let Some(t) = t {
+                t.draw(draw);
             }
-        }
-
-
-
+        });
     }
+
     pub fn update(&mut self) {
-        match self.particle_container {
-            ParticleContainer::Particles(particles) => {
-                for mut particle in particles.iter().filter(|p| p.is_some()).map(|p| p.unwrap()) {
-                    particle.update();
-                }
+        self.particle_container.particles.iter_mut().for_each(|p| {
+            if let Some(p) = p {
+                p.update();
             }
-            ParticleContainer::Divided => {
-                self.north_west.as_mut().unwrap().update();
-                self.north_east.as_mut().unwrap().update();
-                self.south_west.as_mut().unwrap().update();
-                self.south_east.as_mut().unwrap().update();
+        });
+        self.particle_container.sub_trees.iter_mut().for_each(|t| {
+            if let Some(t) = t {
+                t.update();
             }
-        }
+        });
     }
+
     pub fn insert(&mut self, particle: Particle) -> bool {
         if !self.boundary.contains(&particle) {
             return false;
         }
-
-        match self.particle_container {
-            ParticleContainer::Particles(ref mut particles) => {
-                let particle_array = particles.iter_mut().filter(|p| p.is_none()).next();
-                match particle_array {
-                    Some(p) => {
-                        *p = Some(particle);
-                        true
-                    }
-                    None => {
-                        self.subdivide();
-                        self.insert(particle)
-                    }
-                }
-            }
-            ParticleContainer::Divided => {
-                let nw = self.north_west.as_mut().unwrap().insert(particle.clone());
-                let ne = self.north_east.as_mut().unwrap().insert(particle.clone());
-                let sw = self.south_west.as_mut().unwrap().insert(particle.clone());
-                let se = self.south_east.as_mut().unwrap().insert(particle.clone());
-
-                if nw || ne || sw || se {
-                    true
-                } else {
-                    false
-                }
-            }
+        if self.particle_container.particles.iter().filter(|p| p.is_some()).count() < CAPACITY {
+            self.particle_container.particles.iter_mut().filter(|p| p.is_none()).next().unwrap().replace(particle);
+            true
+        } else {
+            self.subdivide();
+            return self.insert(particle);
         }
     }
 
@@ -196,26 +198,22 @@ impl QuadTree {
         let y = self.boundary.center.y;
         let width: f32 = self.boundary.width;
         let height: f32 = self.boundary.height;
-        match self.particle_container {
-            ParticleContainer::Particles(particles) => {
-
-                self.particle_container=ParticleContainer::Divided;
-                particles.iter().filter(|p| p.is_some()).map(|p| p.unwrap()).for_each(|p| {
-                    self.insert(p);
-                });
-            }
-            ParticleContainer::Divided => {} //shouldn't be here?
-        }
-
-        // self.particle_container = ParticleContainer::Divided; //i think this is bug. all the old particles should be redistributed, here they are killed...
-        let north_west = QuadTree::new(Boundary::new(Point2::new(x - width / 4.0, y + height / 4.0), width / 2.0, height / 2.0));
-        let north_east = QuadTree::new(Boundary::new(Point2::new(x + width / 4.0, y + height / 4.0), width / 2.0, height / 2.0));
-        let south_west = QuadTree::new(Boundary::new(Point2::new(x - width / 4.0, y - height / 4.0), width / 2.0, height / 2.0));
-        let south_east = QuadTree::new(Boundary::new(Point2::new(x + width / 4.0, y - height / 4.0), width / 2.0, height / 2.0));
-        self.north_west = Some(Box::new(north_west));
-        self.north_east = Some(Box::new(north_east));
-        self.south_west = Some(Box::new(south_west));
-        self.south_east = Some(Box::new(south_east));
-
+        self.particle_container.sub_trees.push(
+            Some(Box::new(QuadTree::new(Boundary::new(Point2::new(x - width / 4.0, y - height / 4.0), width / 2.0, height / 2.0)))));
     }
+
+    // self.particle_container = ParticleContainer::Divided; //i think this is bug. all the old particles should be redistributed, here they are killed...
+    let north_west = QuadTree::new(Boundary::new(Point2::new(x - width / 4.0, y + height / 4.0), width / 2.0, height / 2.0));
+    let north_east = QuadTree::new(Boundary::new(Point2::new(x + width / 4.0, y + height / 4.0), width / 2.0, height / 2.0));
+    let south_west = QuadTree::new(Boundary::new(Point2::new(x - width / 4.0, y - height / 4.0), width / 2.0, height / 2.0));
+    let south_east = QuadTree::new(Boundary::new(Point2::new(x + width / 4.0, y - height / 4.0), width / 2.0, height / 2.0));
+    self .north_west = Some(Box::new(north_west));
+    self .north_east = Some(Box::new(north_east));
+    self .south_west = Some(Box::new(south_west));
+    self .south_east = Some(Box::new(south_east));
+}
+
+pub fn query(self, particle: &Particle) -> bool {
+    return false;
+}
 }
