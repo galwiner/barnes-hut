8022276432e7f1844f5a0ef3a2b0103ef8bb3494
Joe Lee-Moyet <github@yjo.me>
2023-04-14 20:02:36 +0100

    [joe wip]

--- src/main.rs
+++ src/main.rs
@@ -8,66 +8,67 @@ use crate::quad_tree::Boundary;
 mod quad_tree;
 
 
-const WINDOW_SIZE:f32=800.0;
+const WINDOW_SIZE: f32 = 800.0;
 
 fn main() {
     nannou::app(model).update(update).run()
 }
 
 
-struct Model {
+pub struct Model {
     inspector: Boundary,
-    qt : quad_tree::QuadTree
+    qt: quad_tree::QuadTree,
+    draw_particles: bool,
 }
 
 fn model(app: &App) -> Model {
-    let inspector = Boundary::new(Point2::new(app.mouse.x,app.mouse.y),100 as f32,100 as f32);
-    let mut qt = quad_tree::QuadTree::new(Boundary::new(Point2::new(0.0,0.0),WINDOW_SIZE as f32,WINDOW_SIZE as f32));
+    let inspector = Boundary::new(Point2::new(app.mouse.x, app.mouse.y), 100 as f32, 100 as f32);
+    let mut qt = quad_tree::QuadTree::new(Boundary::new(Point2::new(0.0, 0.0), WINDOW_SIZE as f32, WINDOW_SIZE as f32));
     let _window = app.new_window().size(800, 800)
         .view(view)
         .mouse_pressed(handle_mouse)
         .mouse_moved(handle_mouse_move)
         .key_pressed(handle_key)
         .build().unwrap();
-    for _ in 0..100{
-        qt.insert(quad_tree::Particle::new_random()); }
+    for _ in 0..100 {
+        qt.insert(quad_tree::Particle::new_random());
+    }
 
-    Model{inspector,qt}
+    Model { inspector, qt, draw_particles: true }
 }
 
 
 fn update(app: &App, model: &mut Model, _update: Update) {
     model.qt.update();
-    model.inspector.update(Point2::new(app.mouse.x,app.mouse.y));
+    model.inspector.update(Point2::new(app.mouse.x, app.mouse.y));
 
     let _win_rect = app.window_rect();
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
-
     let draw = app.draw();
-     draw.background().color(BLACK);
-    model.qt.draw(&draw);
+    draw.background().color(BLACK);
+    model.qt.draw(&draw, &model);
     model.inspector.draw(&draw);
     // Write the result of our drawing to the window's frame.
     draw.to_frame(app, &frame).unwrap();
 }
+
 fn handle_mouse(app: &App, model: &mut Model, button: MouseButton) {
-    model.qt.insert(quad_tree::Particle::new(app.mouse.x,app.mouse.y));
+    model.qt.insert(quad_tree::Particle::new(app.mouse.x, app.mouse.y));
 }
 
-fn handle_mouse_move(app: &App,model:&mut Model, _pt: Point2){
+fn handle_mouse_move(app: &App, model: &mut Model, _pt: Point2) {
     // println!("mouse x: {}, mouse y: {}", app.mouse.x, app.mouse.y);
-    model.inspector.update(Point2::new(app.mouse.x,app.mouse.y));
-    let highlighted = model.qt.query(&model.inspector);
-    highlighted.iter().for_each(|p| p.set_color([1.0,0.0,0.0,1.0]));
+    model.inspector.update(Point2::new(app.mouse.x, app.mouse.y));
+    model.qt.mutate_each_in_range(&model.inspector, &mut |p| p.set_color([1.0, 0.0, 0.0, 1.0]));
 }
 
 fn handle_key(app: &App, model: &mut Model, key: Key) {
     match key {
         Key::Space => {
-            model.qt.draw_particles = !model.qt.draw_particles;
-            println!("draw_particles: {}", model.qt.draw_particles)
+            model.draw_particles = !model.draw_particles;
+            println!("draw_particles: {}", model.draw_particles)
         }
         _ => {}
     }
--- src/quad_tree.rs
+++ src/quad_tree.rs
@@ -1,10 +1,12 @@
 extern crate nannou;
 
 use std::fmt;
+use std::ops::Range;
 use nannou::draw::properties::ColorScalar;
 use nannou::rand;
 use nannou::prelude::*;
 use rand_distr::{Distribution, Normal};
+use crate::Model;
 
 const CAPACITY: usize = 4;
 const WINDOW_SIZE: f32 = 800.0;
@@ -18,7 +20,7 @@ pub struct Particle {
     acceleration: Vec2,
     mass: f32,
     radius: f32,
-    pub(crate) color: [ColorScalar;4],
+    pub(crate) color: [ColorScalar; 4],
 }
 
 impl Particle {
@@ -35,7 +37,7 @@ impl Particle {
             acceleration,
             mass,
             radius,
-            color
+            color,
         }
     }
     pub fn new_random() -> Self {
@@ -53,20 +55,19 @@ impl Particle {
             acceleration,
             mass,
             radius,
-            color
+            color,
         }
     }
-    pub fn set_color(&mut self, color: [ColorScalar;4]){
+    pub fn set_color(&mut self, color: [ColorScalar; 4]) {
         self.color = color;
     }
     pub fn draw(&self, draw: &Draw) {
-        println!("color: {:?}", self.color);
+        // println!("color: {:?}", self.color);
         draw.ellipse()
             .x_y(self.position.x, self.position.y)
-            .w_h(self.radius*50.0, self.radius*50.0)
+            .w_h(self.radius * 50.0, self.radius * 50.0)
             .rgba(self.color[0], self.color[1], self.color[2], self.color[3])
             .stroke(rgba(0.0, 0.0, 0.0, 1.0));
-
     }
     pub fn update(&mut self) {
         // self.position += self.velocity;
@@ -79,6 +80,18 @@ pub struct Boundary {
     width: f32,
     height: f32,
 }
+
+// #[derive(Debug, Clone)]
+// pub struct Boundary2 {
+//     x_range: Range<f32>,
+//     y_range: Range<f32>,
+// }
+//
+// impl Boundary2 {
+//     pub fn contains(self, particle: &Particle) -> bool {
+//         self.x_range.contains(&particle.position.x) && self.y_range.contains(&particle.position.y)
+//     }
+// }
 
 impl Boundary {
     pub fn new(center: Point2, width: f32, height: f32) -> Self {
@@ -146,7 +159,6 @@ impl ParticleContainer {
 
 #[derive(Debug)]
 pub struct QuadTree {
-    pub draw_particles: bool,
     pub boundary: Boundary,
     pub particle_container: ParticleContainer,
 }
@@ -160,15 +172,14 @@ impl fmt::Display for QuadTree {
 impl QuadTree {
     pub fn new(boundary: Boundary) -> Self {
         Self {
-            draw_particles: true,
             boundary,
             particle_container: ParticleContainer::new(),
         }
     }
 
-    pub fn draw(&self, draw: &Draw) {
+    pub fn draw(&self, draw: &Draw, model: &Model) {
         self.boundary.draw(draw);
-        if self.draw_particles {
+        if model.draw_particles {
             self.particle_container.particles.iter().for_each(|p| {
                 if let Some(p) = p {
                     p.draw(draw);
@@ -179,7 +190,7 @@ impl QuadTree {
         self.particle_container.sub_trees.iter().for_each(|t| {
             t.iter().for_each(|t| {
                 // println!("draw sub tree");
-                t.draw(draw);
+                t.draw(draw, model);
             });
         });
     }
@@ -257,7 +268,23 @@ impl QuadTree {
         // println!("particles: {:?}", particles);
         particles
     }
+
+    pub fn mutate_each_in_range<F>(&mut self, range: &Boundary, mut operation: &mut F)
+        where F: FnMut(&mut Particle)
+    {
+        if self.boundary.intersects(range) {
+            self.particle_container.particles.iter_mut().flatten()
+                .filter(|particle| range.contains(particle))
+                .for_each(&mut operation);
+
+            self.particle_container.sub_trees.iter_mut().flatten()
+                .for_each(|subtree| {
+                    subtree.mutate_each_in_range(range, operation);
+                });
+        }
+    }
 }
+
 fn fix_overlapping_particles(particle_container: &mut [Option<Particle>; 4], particle: &Particle) {
     particle_container.iter_mut().for_each(|p| {
         if let Some(p) = p {
