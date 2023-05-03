1f6c2b4993b51fec667b610653f1cf1615ae5686
Gal Winer <gal@classiq.io>
2023-05-03 13:42:24 +0300

    fix of rect shape, still broken

--- src/physics/barnes_hut.rs
+++ src/physics/barnes_hut.rs
@@ -51,7 +51,7 @@ where
 impl MassAggregate<Space2D, 4> {
     pub(crate) fn get_bounding_rect(&self) -> Rect {
 
-        Rect::from_xy_wh(self.pivot, vec2(self.width, self.width))
+        Rect::from_xy_wh(self.pivot, vec2(self.width*0.5, self.width*0.5))
     }
 }
 
